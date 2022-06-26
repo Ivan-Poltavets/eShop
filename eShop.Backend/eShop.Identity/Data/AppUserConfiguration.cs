b8063ca97c250f13043c5f5d2d878db9d159da8e
Ivan-Poltavets <poltavets.i291@gmail.com>
2022-06-26 22:20:36 +0300

    Add users store

---
+++ eShop.Backend/eShop.Identity/Data/AppUserConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using eShop.Identity.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eShop.Identity.Data
+{
+    public class AppUserConfiguration : IEntityTypeConfiguration<AppUser>
+    {
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
--- eShop.Backend/eShop.Identity/Program.cs
+++ eShop.Backend/eShop.Identity/Program.cs
@@ -1,14 +1,43 @@
 using eShop.Identity;
+using eShop.Identity.Data;
+using eShop.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var connection = builder.Configuration.GetConnectionString("DbConnection");
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
+{
+    options.UseSqlite(connection);
+});
+
+builder.Services.AddIdentity<AppUser, IdentityRole>(config =>
+{
+    config.Password.RequiredLength = 4;
+    config.Password.RequireDigit = false;
+    config.Password.RequireNonAlphanumeric = false;
+    config.Password.RequireUppercase = false;
+})
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddControllersWithViews();
+
 builder.Services.AddIdentityServer()
+    .AddAspNetIdentity<AppUser>()
     .AddInMemoryApiResources(Configuration.ApiResources)
     .AddInMemoryApiScopes(Configuration.ApiScopes)
     .AddInMemoryIdentityResources(Configuration.IdentityResources)
     .AddInMemoryClients(Configuration.Clients)
     .AddDeveloperSigningCredential();
+
+builder.Services.ConfigureApplicationCookie(config =>
+{
+    config.Cookie.Name = "eShop.Identity.Cookie";
+    config.LoginPath = "/Auth/Login";
+    config.LogoutPath = "/Auth/Logout";
+});
 
 var app = builder.Build();
 
@@ -16,6 +45,20 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
+}
+
+using(var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        var context = serviceProvider.GetRequiredService<AuthDbContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch(Exception exception)
+    {
+        
+    }
 }
 
 app.UseHttpsRedirection();
