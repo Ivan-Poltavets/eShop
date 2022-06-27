d2f2129630a50eddb042d2625e8fa285fd6c32f2
Ivan-Poltavets <poltavets.i291@gmail.com>
2022-06-27 16:58:17 +0300

    Add authentication options

--- eShop.Backend/eShop.API/Program.cs
+++ eShop.Backend/eShop.API/Program.cs
@@ -1,13 +1,36 @@
 using eShop.Persistance;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddPersistance(builder.Configuration);
 builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddCors();
-builder.Services.AddAuthentication();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.AllowAnyOrigin();
+    });
+});
+
+builder.Services.AddAuthentication(config =>
+{
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:/";
+        options.Audience = "eShopWebAPI";
+        options.RequireHttpsMetadata = false;
+    });
+
 builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
@@ -17,6 +40,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 using(var scope = app.Services.CreateScope())
 {
     var serviceProvider = scope.ServiceProvider;
@@ -30,8 +54,9 @@ using(var scope = app.Services.CreateScope())
 
     }
 }
+
 app.UseHttpsRedirection();
-app.UseCors();
+app.UseCors("AllowAll");
 
 app.UseAuthentication();
 app.UseAuthorization();
