fd5dde0f9592010300625415a29e174e23a62d22
Ivan-Poltavets <poltavets.i291@gmail.com>
2022-06-27 20:23:38 +0300

    Add order controller

---
+++ eShop.Backend/eShop.API/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using eShop.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShop.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : BaseController
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+            => _orderService = orderService;
+
+        [HttpGet]
+        public async Task<IActionResult> GetOrders()
+            => Ok(await _orderService.GetOrdersAsync(UserId));
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrder(Guid id)
+            => Ok(await _orderService.GetOrderAsync(id, UserId));
+    }
+}
